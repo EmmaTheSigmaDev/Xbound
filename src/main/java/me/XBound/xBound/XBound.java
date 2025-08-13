67efb63ece0b4f5d5e8f686229a5cc861aa8953d
BetterCore <emmathesigma18@gmail.com>
2025-08-13 18:24:26 -0500

    i fogot again

--- src/main/java/me/XBound/xBound/XBound.java
+++ src/main/java/me/XBound/xBound/XBound.java
@@ -46,8 +46,6 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.*;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
